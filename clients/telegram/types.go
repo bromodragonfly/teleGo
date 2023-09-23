d61766a8cd8223aa30af34929e491a969049c79d
bromodragonfly <d.v-xcc@yandex.ru>
2023-09-23 12:58:08 +0300

    feat: telegram client

--- clients/telegram/types.go
+++ clients/telegram/types.go
@@ -1,5 +1,14 @@
 package telegram
 
+
+// TODO: сделать общую  структуру "BaseResponse" и добавлять информацию для запроса отдельно
+type UpdateSResponse struct{
+	Ok bool `json:"ok"`
+	Result []Update `json:"result"`
+}
+
 type Update struct {
+	ID int `json:"update_id"`
+	Message string `json:"message"`
 
 }
